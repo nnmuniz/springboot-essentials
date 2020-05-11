2b6618d5c59c22024dd06ee086bfd35ee9bb925f
nnmuniz <pcnathalya@hotmail.com>
2020-05-10 23:44:14 -0300

    Springboot essentials aplicacoes rest

---
+++ src/main/java/br/com/springboot/SpringBootEssentialsApplications.java
@@ -0,0 +1,13 @@
+package br.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentialsApplications {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentialsApplications.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/springboot/model/Student.java
@@ -0,0 +1,67 @@
+package br.com.springboot.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Arrays.asList;
+
+public class Student {
+    private int id;
+    private String name;
+    public static List<Student> studentList;
+
+    static {
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+    }
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    private static void studentRepository(){
+        studentList = new ArrayList<>(asList(new Student(1, "Nathalya"), new Student(2,"Muniz"), new Student(3,"Pelicas")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
